bb8cf34a094e9ed115c16148abc0d2e47fb60bbe
Sribarathvajasarma <gsribarathvajasarma@gmail.com>
2023-12-31 09:25:25 +0530

    Revert "Adding enrollment service and Availablity service"
    This reverts commit f4a32f40749d33400e284f653369b543a9b861ad.

--- enrollment-service/src/main/java/com/project/enrollmentservice/repository/EnrollmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.enrollmentservice.repository;
-
-import com.project.enrollmentservice.model.Enrollment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
-}
